3c9eb4024b498c651c1a623b10157241355a7a3e
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-09-26 15:11:44 +0400

    fix: Trim headers and remove carriage returns in CSV parsing

--- src/controllers/riversController.js
+++ src/controllers/riversController.js
@@ -44,7 +44,7 @@ class RiversController {
    */
   parseCSVData(csvContent) {
     const lines = csvContent.trim().split('\n');
-    const headers = lines[0].split(',');
+    const headers = lines[0].split(',').map(header => header.trim().replace(/\r/g, ''));
     
     return lines.slice(1).map((line, index) => {
       const values = this.parseCSVLine(line);
